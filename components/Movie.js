7323e4fa487da6d007dd619813a99f3abe620a67
Bruno G Alcantara <inovox@gmail.com>
2020-03-22 13:47:26 -0300

    ajuste no posicionamento do link de exibicao dos detalhes

--- components/Movie.js
+++ components/Movie.js
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default ({ movie }) => (
   <div>
-    <p>Título: {movie.title}</p>
-    <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
-      <a>Detalhes</a>
-    </Link>
+    <p>
+      Título: {movie.title}
+      <Link href="/movies/[id]" as={`/movies/${movie.id}`}><a>Detalhes</a></Link>
+    </p>
   </div>
 )
